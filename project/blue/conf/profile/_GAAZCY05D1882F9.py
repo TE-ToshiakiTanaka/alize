6f8e3388f1e1cf0fcc23f2484b6823e9df41f2df
Toshiaki Tanaka <Toshiaki.Tanaka@jp.sony.com>
2017-05-30 23:40:14 +0900

    wip

---
+++ project/blue/conf/profile/_GAAZCY05D1882F9.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+PATH = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+if not PATH in sys.path:
+    sys.path.insert(0, PATH)
+
+import android_base
+
+class _GAAZCY05D1882F9(android_base.Android):
+    SERIAL = "GAAZCY05D1882F9"
+    TMP_PICTURE = "%s_TMP.png" % SERIAL
+    IP = ""
+    PORT = ""
+
+    NAME = "ASUS ZenFone"
+    WIDTH = "1920"
+    HEIGHT = "1080"
+    LOCATE = "H"
+
+    EXERCISES_X = "1590"
+    EXERCISES_Y = "390"
+    EXERCISES_WIDTH = "180"
+    EXERCISES_HEIGHT = "120"
+
+    FORMATION_X = "320"
+    FORMATION_Y = "375"
+    FORMATION_WIDTH = "445"
+    FORMATION_HEIGHT = "115"
+
+    DOCKING_X = "800"
+    DOCKING_Y = "228"
+    DOCKING_WIDTH = "300"
+    DOCKING_HEIGHT = "100"
+
+
+if __name__ == "__main__":
+    print(eval("_GAAZCY05D1882F9.%s" % "TMP_PICTURE"))
